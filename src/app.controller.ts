b6acb0477984aafc410abdf25d8b9f9111e39b53
Gabe Hargrave <gkhargrave@gmail.com>
2020-04-03 10:09:13 -0700

    Initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get } from '@nestjs/common'
+import { AppService } from './app.service'
+
+@Controller('api/v1')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello()
+  }
+
+  @Get('hello')
+  hello() {
+    return {
+      data: 'oh hai there',
+    }
+  }
+}
